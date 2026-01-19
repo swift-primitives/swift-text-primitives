d197f02ba8ffdaf7b4a0b43ff593a2ff049000d7
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-19 22:24:31 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-text-primitives",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26)
+    ],
+    products: [
+        .library(
+            name: "Text Primitives",
+            targets: ["Text Primitives"]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Text Primitives"
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
+    let settings: [SwiftSetting] = [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableExperimentalFeature("Lifetimes"),
+        .strictMemorySafety()
+    ]
+    target.swiftSettings = (target.swiftSettings ?? []) + settings
+}
